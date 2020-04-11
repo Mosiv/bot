3e664dbab632dbf7924a3abc1a660420f1761573
Mosiv <aqlpp1@yandex.ru>
2020-04-11 21:17:29 +0300

    file

---
+++ main.py
@@ -0,0 +1,9 @@
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+TG_TOKEN = ""
+
+def main()
